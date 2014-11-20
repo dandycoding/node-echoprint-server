401d61212ad4baccd28c2c8bc57202842b8b5c12
Seth Hochberg <seth@sethhochberg.com>
2014-11-19 22:07:01 -0500

    treat track length / duration as the same

--- controllers/api.js
+++ controllers/api.js
@@ -12,29 +12,29 @@ exports.query = function(req, res) {
   var code = url.query.code;
   if (!code)
     return server.respond(req, res, 500, { error: 'Missing code' });
-  
+
   var codeVer = url.query.version;
   if (codeVer != config.codever)
     return server.respond(req, res, 500, { error: 'Missing or invalid version' });
-  
+
   fingerprinter.decodeCodeString(code, function(err, fp) {
     if (err) {
       log.error('Failed to decode codes for query: ' + err);
       return server.respond(req, res, 500, { error: 'Failed to decode codes for query: ' + err });
     }
-    
+
     fp.codever = codeVer;
-    
+
     fingerprinter.bestMatchForQuery(fp, config.code_threshold, function(err, result) {
       if (err) {
         log.warn('Failed to complete query: ' + err);
         return server.respond(req, res, 500, { error: 'Failed to complete query: ' + err });
       }
-      
+
       var duration = new Date() - req.start;
       log.debug('Completed lookup in ' + duration + 'ms. success=' +
         !!result.success + ', status=' + result.status);
-      
+
       return server.respond(req, res, 200, { success: !!result.success,
         status: result.status, match: result.match || null, custom_id: result.custom_id });
     });
@@ -47,11 +47,11 @@ exports.query = function(req, res) {
 exports.ingest = function(req, res) {
   var code = req.body.code;
   var codeVer = req.body.metadata.version;
-  var length = req.body.length;
+  var length = req.body.length || req.body.metadata.duration;
   var track = req.body.track;
   var artist = req.body.artist;
   var custom_id = req.body.custom_id;
-  
+
   if (!code)
     return server.respond(req, res, 500, { error: 'Missing "code" field' });
   if (!codeVer)
@@ -60,29 +60,29 @@ exports.ingest = function(req, res) {
     return server.respond(req, res, 500, { error: 'Version "' + codeVer + '" does not match required version "' + config.codever + '"' });
   if (isNaN(parseInt(length, 10)))
     return server.respond(req, res, 500, { error: 'Missing or invalid "length" field' });
-  
+
   fingerprinter.decodeCodeString(code, function(err, fp) {
     if (err || !fp.codes.length) {
       log.error('Failed to decode codes for ingest: ' + err);
       return server.respond(req, res, 500, { error: 'Failed to decode codes for ingest: ' + err });
     }
-    
+
     fp.codever = codeVer;
     fp.track = track;
     fp.length = parseInt(length, 10);
     fp.artist = artist;
     fp.custom_id = custom_id;
-    
+
     fingerprinter.ingest(fp, function(err, result) {
       if (err) {
         log.error('Failed to ingest track: ' + err);
         return server.respond(req, res, 500, { error: 'Failed to ingest track: ' + err });
       }
-      
+
       var duration = new Date() - req.start;
       log.debug('Ingested new track in ' + duration + 'ms. track_id=' +
         result.track_id + ', artist_id=' + result.artist_id);
-      
+
       result.success = true;
       return server.respond(req, res, 200, result);
     });
